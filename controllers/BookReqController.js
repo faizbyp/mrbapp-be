1f52c99128c7a6216cfdcb17eb1491420960549a
faizbyp <faizbyp@gmail.com>
2024-07-12 16:03:56 +0700

    add: approval status filter on showallbook

--- controllers/BookReqController.js
+++ controllers/BookReqController.js
@@ -168,9 +168,12 @@ const BookReqController = {
     try {
       await client.beginTransaction();
       const book_date = req.query.book_date || null;
+      const approval = req.query.approval || null;
       const showall = await client.query(
-        "SELECT req_book.*, mst_user.username FROM req_book LEFT JOIN mst_user ON req_book.id_user = mst_user.id_user WHERE (req_book.book_date = ? OR ? IS NULL) ORDER BY req_book.id DESC",
-        [book_date, book_date]
+        `SELECT req_book.*, mst_user.username FROM req_book LEFT JOIN mst_user ON req_book.id_user = mst_user.id_user
+        WHERE (req_book.book_date = ? OR ? IS NULL) AND (req_book.approval = ? OR ? IS NULL)
+        ORDER BY req_book.id DESC`,
+        [book_date, book_date, approval, approval]
       );
       await client.commit();
       res.status(200).send({ data: showall[0] });
