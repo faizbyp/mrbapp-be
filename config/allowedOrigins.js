b1889650c54ea7fd838a69abd3bb77b570f50086
faizbyp <faizbyp@gmail.com>
2024-08-16 08:44:44 +0700

    add: whitelist domain origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const origins = [
   "https://localhost:3000",
   "http://localhost:3000",
   "https://mrbapp.nematodez.my.id",
+  "https://roomeet.gamasap.com",
+  "https://roomeet-dev.gamasap.com",
   "http://172.30.6.167:3000",
 ];
 
