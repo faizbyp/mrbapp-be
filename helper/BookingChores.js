c8f0d12cd86eda2f53419d3c361f672e1066bafb
faizbyp <faizbyp@gmail.com>
2024-08-21 17:15:32 +0700

    fix: user penalty on bookingchores if pending, user also get penalty (unwanted, fixed)

--- helper/BookingChores.js
+++ helper/BookingChores.js
@@ -37,40 +37,40 @@ BookingChores.userPenalty = async function (usersId, client) {
   }
 };
 
-BookingChores.CleanUp = async () => {
+BookingChores.Penalty = async () => {
   const Client = new DbConn();
   const client = await Client.initConnection();
   const penUser = new Set(); // Using Set to automatically handle unique user ids
-  const idBook = new Set();
   try {
     await client.beginTransaction();
-    const res = await client.query(`
+    const pen = await client.query(`
       SELECT
-        id_book, id_user, id_ruangan, book_date, time_start, time_end, is_active
+        id_book, id_user, id_ruangan, book_date, time_start, time_end, is_active, approval, check_in, check_out
       FROM
         req_book BOOK 
       WHERE
         TIMESTAMP(CONCAT( BOOK.book_date, ' ', BOOK.time_end )) + INTERVAL 15 MINUTE < CONVERT_TZ(NOW(), '+00:00', '+07:00')
         AND
-        IS_ACTIVE = 'T'
+        is_active = 'T'
+        AND
+        approval = 'approved'
         AND
         ((check_in = 'T' AND check_out = 'F') OR (check_in = 'F' AND check_out = 'F'))
       `);
-    const expiredBook = res[0];
-    if (expiredBook.length === 0) {
-      return "No expired booking, everything is clear";
+    const penaltyUser = pen[0];
+
+    if (penaltyUser.length === 0) {
+      return "No user penalty";
     }
-    expiredBook.forEach((item) => {
-      idBook.add(item.id_book);
+
+    penaltyUser.forEach((item) => {
       penUser.add(item.id_user);
     });
+
     let usersPen = [];
-    let bookId = [];
+
     penUser.forEach((item) => {
       usersPen.push(item);
-    });
-    idBook.forEach((item) => {
-      bookId.push(item);
     });
 
     const uPenHolder = usersPen.map(() => "?").join(",");
@@ -84,6 +84,49 @@ BookingChores.CleanUp = async () => {
     let userPen = resuser[0].map((item) => item.id_user);
 
     await BookingChores.userPenalty(userPen, client);
+
+    await client.commit();
+    return "success add penalty";
+    // const updateReqBook = Promise.all(promise);
+  } catch (error) {
+    await client.rollback();
+    console.error(error);
+  } finally {
+    client.release();
+  }
+};
+
+BookingChores.CleanUp = async () => {
+  const Client = new DbConn();
+  const client = await Client.initConnection();
+  const idBook = new Set(); // Using Set to automatically handle unique user ids
+  try {
+    await client.beginTransaction();
+    const expired = await client.query(`
+      SELECT
+        id_book, id_user, id_ruangan, book_date, time_start, time_end, is_active
+      FROM
+        req_book BOOK 
+      WHERE
+        TIMESTAMP(CONCAT( BOOK.book_date, ' ', BOOK.time_end )) + INTERVAL 15 MINUTE < CONVERT_TZ(NOW(), '+00:00', '+07:00')
+        AND
+        IS_ACTIVE = 'T'
+      `);
+    const expiredBook = expired[0];
+
+    if (expiredBook.length === 0) {
+      return "No expired booking, everything is clear";
+    }
+
+    expiredBook.forEach((item) => {
+      idBook.add(item.id_book);
+    });
+
+    let bookId = [];
+
+    idBook.forEach((item) => {
+      bookId.push(item);
+    });
 
     const bIdHolder = bookId.map(() => "?").join(",");
     const resUpBook = await client.query(
@@ -104,21 +147,12 @@ BookingChores.CleanUp = async () => {
   }
 };
 
-// BookingChores.EmailReminder = async () => {
-//   const Client = new DbConn();
-//   const client = await Client.initConnection();
-//   try {
-
-//   } catch (error) {
-
-//   }
-// }
-
 setInterval(async () => {
   try {
+    const penaltyRes = await BookingChores.Penalty();
     const result = await BookingChores.CleanUp();
     await NotificationManager.CleanUpCron();
-    console.log(result);
+    console.log(result, penaltyRes);
   } catch (error) {
     console.log(error);
   }
